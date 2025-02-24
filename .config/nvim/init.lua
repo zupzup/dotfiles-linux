a86ea6314d66ee28647c59ac76e2119836485b6c
Mario Zupan <mario@zupzup.org>
2025-02-24 13:01:40 +0100

    quickfix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -263,7 +263,6 @@ require('lazy').setup({
             vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
             vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
             vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
             -- Use LspAttach autocommand to only map the following keys
             -- after the language server attaches to the current buffer
@@ -384,7 +383,6 @@ vim.keymap.set('n', '<leader>ust', ':set softtabstop=2 <bar> :set shiftwidth=2 <
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- General Settings
 vim.opt.number = true
